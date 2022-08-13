0b745a2b4ab8a08b1e7855dbb209a214595163db
Parsa Shobany <Parsa_sh@hotmail.co.uk>
2022-08-13 13:56:31 +0100

    London Parks App

---
+++ LondonParks/ParkDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  ParkDetailsView.swift
+//  LondonParks
+//
+//  Created by Parsa Shobany on 13/08/2022.
+//
+
+import SwiftUI
+
+
+struct ParkDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ParkDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParkDetailsView()
+    }
+}
---
+++ LondonParks/Parks.swift
@@ -0,0 +1,34 @@
+//
+//  Parks.swift
+//  LondonParks
+//
+//  Created by Parsa Shobany on 13/08/2022.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Parks: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image{
+        Image(imageName)}
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)}
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
