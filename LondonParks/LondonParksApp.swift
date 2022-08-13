bb412b52fea046aab69cf5c7cb887d0c7ffffbdc
Parsa Shobany <Parsa_sh@hotmail.co.uk>
2022-08-13 13:44:54 +0100

    Initial Commit

---
+++ LondonParks/LondonParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LondonParksApp.swift
+//  LondonParks
+//
+//  Created by Parsa Shobany on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LondonParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
